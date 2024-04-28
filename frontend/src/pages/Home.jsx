25faef23a6788b47aa8eb6d828379da834068330
anujmishra <anujmishra2702@gmail.com>
2024-04-28 18:05:51 +0530

    updated frontend css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,19 @@ import {motion } from 'framer-motion' ;
 import { FaArrowRightLong } from "react-icons/fa6";
 import  {omega} from "../assets/index";
 import { omega2 } from "../assets/index";
+import Markey from '../components/Markey';
+import Aboutlanding from '../components/Aboutlanding'
+import Line from '../components/line' 
+
 
 export default function Home() {
   return (
-   <div className='w-full h-screen bg-white pt-1'>
+   <div data-scroll data-scroll-section data-scroll-speed=".6" className='w-full h-screen bg-white pt-1'>
+    <div className='absolute blur-sm w-[32vh] h-[32vh] bg-red-500 top-[25vh] right-[15vh]'>
+      <img src="https://qrcodedynamic.com/themes/altum/assets/images/qr_code.svg" alt="" />
+    </div>
+      <h1 className='absolute top-[37vh] right-[20vh] uppercase text-[2rem] font-bold text-red-500 bg-white p-2 '>qr scan </h1>
+    {/* <div className='absolute top-[60vh] right-[23vh] px-5 py-2 border-[2px] border-zinc-400 font-light text-md uppercase rounded-full'>scan qr</div> */}
         <div className="textstructure mt-40 px-20">
             {['welcome' , 'we our' , 'omega squad'].map((items , index) => {
                 return <div className="masker ">
@@ -22,23 +31,25 @@ export default function Home() {
                     </div>
                 
             </div>
+            
             })}
             
            
         </div>
-        <div className='three border-t-[1px] border-zinc-800 mt-20 flex justify-between items-center py-5 px-20'>
+        <div className=' three border-t-[1px] border-zinc-800 mt-20 flex justify-between items-center py-10 px-20'>
             {['for public and pubvate companies' , 'from the first pitch to ipo'].map((item , index ) => (
                 <p className='text-md font-light tracking-tight leading-none'>{item}</p>
             ))}
-            <div className='start flex items-center gap-5'>
-                <div className='px-5 py-2 border-[2px] border-zinc-400 font-light text-md uppercase rounded-full'>start the project</div>
-                <div className='w-10 h-10 flex items-center justify-center border-[2px] border-zinc-400 rounded-full '>
+                {/* <div className='w-10 h-10 flex items-center justify-center border-[2px] border-zinc-400 rounded-full '>
                    <span className='rotate-[45deg]'>
                    <FaArrowRightLong />
                     </span> 
-                </div>
-            </div>
+                </div> */}
         </div>
+        <Markey />
+      <Aboutlanding />
+        
     </div>
+    
   );
 }

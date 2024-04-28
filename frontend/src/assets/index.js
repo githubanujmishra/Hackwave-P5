ac0939f095dc18a756a6294a6f072df482a3ef09
anujmishra <anujmishra2702@gmail.com>
2024-04-28 10:53:52 +0530

    Update frontend

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,9 +1,13 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import omega from './omega.png' ; 
+import omega2 from './omega2.png'
 
 export {
   download,
   logo,
   preview,
+  omega,
+  omega2
 };
